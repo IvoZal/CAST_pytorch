b1706073ef4f0c5f3e66e767a043018f0aacef18
221032 <221032@vut.cz>
2024-04-22 21:06:45 +0200

    Style path added as parameter.

--- data/unaligned_dataset.py
+++ data/unaligned_dataset.py
@@ -26,7 +26,7 @@ class UnalignedDataset(BaseDataset):
         BaseDataset.__init__(self, opt)
         if opt.phase == "test":
             self.dir_A = os.path.join(opt.dataroot, "content")
-            self.dir_B = os.path.join(opt.dataroot, "styles")
+            self.dir_B = os.path.join(opt.stylepath)
         else:
             self.dir_A = os.path.join(opt.dataroot, opt.phase + 'A')  # create a path '/path/to/data/trainA'
             self.dir_B = os.path.join(opt.dataroot, opt.phase + 'B')  # create a path '/path/to/data/trainB'
@@ -53,10 +53,11 @@ class UnalignedDataset(BaseDataset):
             B_paths (str)    -- image paths
         """
         A_path = self.A_paths[index % self.A_size]  # make sure index is within then range
-        if self.opt.serial_batches:   # make sure index is within then range
-            index_B = index % self.B_size
-        else:   # randomize the index for domain B to avoid fixed pairs.
-            index_B = random.randint(0, self.B_size - 1)
+        index_B = 1
+        # if self.opt.serial_batches:   # make sure index is within then range
+        #     index_B = index % self.B_size
+        # else:   # randomize the index for domain B to avoid fixed pairs.
+        #     index_B = random.randint(0, self.B_size - 1)
         B_path = self.B_paths[index_B]
         A_img = Image.open(A_path).convert('RGB')
         B_img = Image.open(B_path).convert('RGB')
